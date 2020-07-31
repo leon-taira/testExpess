9bab9ab2fee490b8c7762e328ac4e90fba3976a8
tennis4leon <39071014+tennis4leon@users.noreply.github.com>
2020-07-31 20:34:18 +0900

    First Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/friends", function (req, res) {
+  let friends = ["Mark", "Tony", "Haruki", "Bob"];
+  res.render("friends", { friends: friends });
+});
+
+app.listen(3000, function () {
+  console.log("Server Ported on 3000");
+});
